27fd5b588fb91366398dae8f0196abf9fff42579
HS H <3262398@naver.com>
2023-04-28 14:35:10 +0900

    0428

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 import  'bootstrap/dist/css/bootstrap.min.css' ;
 import './App.css';
 
+// 리액트 슬릭을 사용하기 위해 css추가
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 // 라우터를 사용할때는 BrowserRouter를 들고와서 사용
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -14,13 +18,20 @@ import LoginForm from './page/LoginForm'
 import NavHeader from './components/NavHeader';
 import { useState } from 'react';
 
+// 이 값이 바뀌었을때 화면에 출력할 필요가 없으므로
+// 컴포넌트 밖에 작성을 하여 값이 바뀔수 있도록 한다
+
+// * 컴포넌트 안에서 작성하면 업데이트 할때마다 값이 고정됨
+// * 값이 바뀔때 마다 화면을 바꾸고 싶으면 useState() 사용
+let id = 3;
+
 function App() {
   // 데이터를 하위 컴포넌트에 전달하기위해서
   // 상위 컴포넌트에서 데이터를 작성하고 props값을 전달한다
   // 상위 데이터값을 하위에서 공유하기때문에 동일한 값이 출력
 
   // 유저 데이터 
-  // 처음 값 null, "", [], 더미데이터로 내용 확인 후 선택
+  // 처음 값 null, "",[], 더미데이터로 내용 확인 후 선택
   // 처음 값을 null 두었을때 주의점
   // null과 undefined는 속성을 가질수 없기때문에
   // 속성에 접근하면 오류가 난다.
@@ -36,21 +47,26 @@ function App() {
   const [boardlist, setBoardlist] = useState(
     [
       {
-        id : 1,
-        title : "첫번째 게시물",
-        context : "게시물의 내용을 작성합니다",
+        id:1,
+        title : " 첫번째 게시물",
+        content : "게시물의 내용을 작성합니다",
         name : "green",
         date : "2023-04-27"
       },
       {
-        id : 2,
-        title : "두번째 게시물",
-        context : "게시물의 내용을 작성합니다",
+        id:2,
+        title : " 두번째 게시물",
+        content : "게시물의 내용을 작성합니다",
         name : "blue",
         date : "2023-04-27"
       }
     ]
   );
+
+  // id값을 1씩 증가하는 함수
+  const addId = ()=>{
+    id = id +1;
+  }
 
 
   // 주소와 페이지(컴포넌트 연결)
@@ -64,8 +80,11 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/boardlist' element={<Boardlist boardlist={boardlist} />}/>
-          <Route path='/boardlist/:id' element={<Board boardlist={boardlist} />} errorElement={<Home />} />
-          <Route path='/boardform' element={<BoardForm />}/>
+          <Route path='/boardlist/:id' element={<Board boardlist={boardlist} />} />
+          <Route path='/boardform' element={<BoardForm 
+                                              setBoardlist={setBoardlist} 
+                                              boardlist={boardlist} 
+                                              user={user} id={id} addId={addId} />}/>
 
           <Route path='/loginform' element={<LoginForm setUser={setUser} />}/>
         </Routes>
--- src/page/Boardlist.jsx
+++ src/page/Boardlist.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import BoardCard from '../components/BoardCard'
+import Button from 'react-bootstrap/esm/Button'
+import { useNavigate } from 'react-router-dom'
 
 export default function Boardlist({boardlist}) {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      { // 배열을 이용하여 화면에 출력할 수 있다
-        // map() : return 값을 배열로 전달
-        // {}나에서 return 값은 화면에 바로 출력된다
-        boardlist.map((board)=>(
-          // props 값으로 전달할 때,
+    <div >
+      <Button 
+      onClick={()=>{navigate('/boardform')}}>
+        게시글 작성
+      </Button>
+      { // 배열을 이용하여 화면에 출력할수 있다
+        // map() : return 값을 배열로 전달 
+        // {}안에서 return 값은 화면에 바로 출력된다
+        boardlist.map((board)=>( 
+          // props 값으로 전달할때,
           // 전달되는 값이 배열인지 객체인지 정하기
-          // 보통은 객체값을 전달하여
+          // 보통은 객체값을 전달하여 
           // 배열에 있는 내용을 출력
-          <div key={board.id}><BoardCard board={board}/></div>
+          <BoardCard board={board} key={board.id}/>
         ))
       }
     </div>
